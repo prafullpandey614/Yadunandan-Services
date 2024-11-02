c7e44c4d780b9b51e7236a7a582bd09434afb8d8
prafullpandey614 <prafullpandey614@gmail.com>
2024-11-02 15:02:14 +0530

    update settings

--- yandunandan/settings.py
+++ yandunandan/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': str(os.getenv('USER')),
         'PASSWORD': str(os.getenv('PASSWORD')),
         'HOST': str(os.getenv('HOST')),
-        'PORT': str(os.getenv('PORT'))
+        'PORT': (os.getenv('PORT'))
     }
 }
 
